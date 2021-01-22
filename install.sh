96cf392e8cdca7f3f99bca7fad640e685f7ec327
Richard Leopold <richy.leopold@outlook.com>
2021-01-22 13:11:13 +0100

    updated install script

--- install.sh
+++ install.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
-hashOutput=$(hash dotnet)
-netCoreDownloadFileName="aspnetcore-runtime-5.0.0-linux-arm64.tar.gz"
+userName=$USER
+homeFolder="/home/"$userName"/"
 
 dotnetPath="/usr/share/dotnet"
+installPath=$homeFolder"/lumin/"
+
+luminConfigName="lumin.config"
+luminConfigPath=$homeFolder".luminConfig/"$luminConfigName
+
+deviceRulesPath="/etc/udev/rules.d/"
+spiDeviceRule=$deviceRulesPath"50-spi.rules"
+
+serverServiceName="LuminServerService.service"
+serverServicePath="/etc/systemd/system/"$serverServiceName
+
+hashOutput=$(hash dotnet)
+netCoreDownloadFileName="aspnetcore-runtime-5.0.0-linux-arm64.tar.gz"
 
 discoveryPort=8888
 signalPort=5000
@@ -26,28 +39,44 @@ echo dotNet path is at: $dotnetPath
 ufw allow $discoveryPort
 ufw allow $signalPort
 
-#write config file to: ~/.luminConfig/lumin.config
+#write config file to: luminConfigPath
 
-#CONFIG FILE Start
-#-------------------------------------------
-	#Name of the Led Client
-	Name=Bed Room
-	#Number of LEDs at the LedClient side
-	LedCount=58
-	#Time in hours for auto off timer
-	AutoOffTime=2
-	#Discovery Port 
-	DiscoveryPort=8080
-#-------------------------------------------
-#CONFIG FILE End
+echo '#CONFIG FILE Start' > $luminConfigPath
+echo '#-------------------------------------------' >> $luminConfigPath
+echo '#Name of the Led Client' >> $luminConfigPath
+echo 'Name=Bed Room' >> $luminConfigPath
+echo '#Number of LEDs at the LedClient side' >> $luminConfigPath
+echo 'LedCount=58' >> $luminConfigPath
+echo '#Time in hours for auto off timer' >> $luminConfigPath
+echo 'AutoOffTime=2' >> $luminConfigPath
+echo '#Discovery Port' >> $luminConfigPath
+echo 'DiscoveryPort=8080' >> $luminConfigPath
+echo '#-------------------------------------------' >> $luminConfigPath
+echo '#CONFIG FILE End' >> $luminConfigPath
 
 #write GPIO and SPI files, add users to enable SPI and GPIO | Path: /etc/udev/rules.d/
+groupadd spiuser
+adduser "$USER" spiuser
+
+echo 'SUBSYSTEM=="spidev", GROUP="spiuser", MODE="0660"' > $spiDeviceRule
 
 #install "libgpiod" --> see here: https://ubuntu.pkgs.org/20.04/ubuntu-universe-amd64/libgpiod-dev_1.4.1-4_amd64.deb.html
 apt install libgpiod2 -y
 
-# Create Service File / Enable and Start Service (systemctl)
+# Create Server Service File / Enable and Start Service (systemctl)
+echo '[Unit]' > $serverServicePath
+echo 'Description=Lumin Service in .NET' >> $serverServicePath
+echo ''
+echo '# Location:' >> $serverServicePath
+echo '# /etc/systemd/system/LuminServerService.service' >> $serverServicePath
+echo ''
+echo '[Service]' >> $serverServicePath
+echo 'Type=simple' >> $serverServicePath
+echo 'WorkingDirectory='$installPath >> $serverServicePath
+echo 'ExecStart='$dotnetPath'/dotnet '$installPath'LuminServer.dll' >> $serverServicePath
+echo 'User=ubuntu' >> $serverServicePath
+echo ''
+echo '[Install]' >> $serverServicePath
+echo 'WantedBy=multi-user.target' >> $serverServicePath
 
-# possibility 1:
-echo "line 1" >> greetings.txt
-echo "line 2" >> greetings.txt
+systemctl enable $serverServiceName
