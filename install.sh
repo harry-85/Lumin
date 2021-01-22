82cf12c9b8a007f754c91afbed8e838c84b9c5a8
Richard Leopold <richy.leopold@outlook.com>
2021-01-22 13:35:30 +0100

    updated install script

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+clientName="Bed Room"
+ledCount=58
+
 userName=$USER
 homeFolder="/home/"$userName"/"
 
@@ -15,13 +18,13 @@ spiDeviceRule=$deviceRulesPath"50-spi.rules"
 serverServiceName="LuminServerService.service"
 serverServicePath="/etc/systemd/system/"$serverServiceName
 
+discoveryPort=8080
+signalPort=5000
+
 hashOutput=$(hash dotnet)
 netCoreDownloadFileName="aspnetcore-runtime-5.0.0-linux-arm64.tar.gz"
 
-discoveryPort=8888
-signalPort=5000
-
-#install dotNet 5.0
+#check and install dotNet 5.0
 if [[ $hashOutput != "" ]]
 then
 	wget https://download.visualstudio.microsoft.com/download/pr/ac555882-afa3-4f5b-842b-c4cec2ae0e90/84cdd6d47a9f79b6722f0e0a9b258888/aspnetcore-runtime-5.0.0-linux-arm64.tar.gz
@@ -40,24 +43,22 @@ ufw allow $discoveryPort
 ufw allow $signalPort
 
 #write config file to: luminConfigPath
-
 echo '#CONFIG FILE Start' > $luminConfigPath
 echo '#-------------------------------------------' >> $luminConfigPath
 echo '#Name of the Led Client' >> $luminConfigPath
-echo 'Name=Bed Room' >> $luminConfigPath
+echo 'Name='$clientName >> $luminConfigPath
 echo '#Number of LEDs at the LedClient side' >> $luminConfigPath
-echo 'LedCount=58' >> $luminConfigPath
+echo 'LedCount='$ledCount >> $luminConfigPath
 echo '#Time in hours for auto off timer' >> $luminConfigPath
 echo 'AutoOffTime=2' >> $luminConfigPath
 echo '#Discovery Port' >> $luminConfigPath
-echo 'DiscoveryPort=8080' >> $luminConfigPath
+echo 'DiscoveryPort='$discoveryPort >> $luminConfigPath
 echo '#-------------------------------------------' >> $luminConfigPath
 echo '#CONFIG FILE End' >> $luminConfigPath
 
 #write GPIO and SPI files, add users to enable SPI and GPIO | Path: /etc/udev/rules.d/
 groupadd spiuser
 adduser "$USER" spiuser
-
 echo 'SUBSYSTEM=="spidev", GROUP="spiuser", MODE="0660"' > $spiDeviceRule
 
 #install "libgpiod" --> see here: https://ubuntu.pkgs.org/20.04/ubuntu-universe-amd64/libgpiod-dev_1.4.1-4_amd64.deb.html
@@ -79,4 +80,8 @@ echo ''
 echo '[Install]' >> $serverServicePath
 echo 'WantedBy=multi-user.target' >> $serverServicePath
 
+#Enable the Server Service
 systemctl enable $serverServiceName
+echo $serverServiceName' is enabled' 
+echo '1. Copy Files to: '$installPath 
+echo '2. Execute: systemctl start '$serverServiceName
